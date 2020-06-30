dd5f3618f4ad03f20e05056c7d9e604a40922de3
jisshub <jissmon476@gmail.com>
2020-06-30 09:46:04 +0530

    averageRating not saved to db

--- models/Review.js
+++ models/Review.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Bootcamp = require("./Bootcamp");
 
 // define a Schema for review
 const ReviewSchema = mongoose.Schema({
@@ -33,6 +34,50 @@ const ReviewSchema = mongoose.Schema({
         required: true
     }
 });
+
+// find average of all ratings in a bootcamp.
+ReviewSchema.statics.getAverageRating = async function (bootcampId) {
+    const obj = await this.aggregate([
+            // match the bootcamp field in Reiview model with bootcampId
+            {
+                $match: {
+                    bootcamp: bootcampId
+                }
+            },
+            // group the id and averageRating
+            {
+                $group: {
+                    _id: '$bootcamp',
+                    averageRating: {
+                        $avg: '$rating'
+                    }
+                }
+            }
+        ]
+        // we gets an array of one object with bootcamp id and averageRating.
+    );
+    // save to db,
+    try {
+        // find bootcamp and update it by adding averageRating field
+        this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+            averageRating: obj[0].averageRating
+        })
+    } catch (error) {
+        console.log(error);
+    }
+
+    console.log(obj); // obj is an array of single object
+}
+
+// call static method after saving to db & before removing from db
+ReviewSchema.post("save", function () {
+    // 
+    this.constructor.getAverageRating(this.bootcamp)
+})
+
+ReviewSchema.pre("remove", function () {
+    this.constructor.getAverageRating(this.bootcamp)
+})
 
 // export the schema
 module.exports = mongoose.model("Review", ReviewSchema)
